0ac7493bf8123490735bc1d2c862530e7449ac43
arbuuuud <arief.setiabudi2010@gmail.com>
2016-06-05 14:26:14 +0800

    bug fix

---
+++ storage/cms/cache/15/77/enquire.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5753a6f0dace7_1805227188Class extends \Cms\Classes\PageCode
+{
+
+}
---
+++ storage/cms/cache/87/21/category.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5753a6f0d8b80_1156560546Class extends \Cms\Classes\LayoutCode
+{
+
+}
